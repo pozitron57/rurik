49a7561db943252f13aeca89227970387c3ac05c
lis <lis@lis.(null)>
2015-03-22 22:59:57 +0300

    Paths and syntax for json_gen.py and tree.json

--- json_gen.py
+++ json_gen.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
-
+#coding=utf8
 
 import json, re
-
 
 # Destination object
 tree = {}
@@ -13,22 +12,39 @@ subtrees = [None]*100
 # Read source file
 with open("source.data") as fp:
     for line in fp:
+
         # Skip empty and commented lines
-        if len(line) <= 1 or line[0] == "#":
+        v = re.match(r'^\s*#', line)
+        if len(line) <= 1 or v:
             continue
 
         # Count number of tabs
         m = re.match(r'^(\s*)(.+)', line)
         tabs = len(m.group(1)) // 4
 
-        # Get name from line:
-        name = m.group(2)
+        ### Read parameters of a guy
+        groups =  line.split('|')
+        name            = '|'.join(groups[0:1]).strip()
+        line_width      = '|'.join(groups[1:2]).strip()
+        line_color      = '|'.join(groups[2:3]).strip() 
+        radius          = '|'.join(groups[3:4]).strip()
+        granica         = '|'.join(groups[4:5]).strip()
+#        birth           = '|'.join(groups[5:6]).strip()
+#        death           = '|'.join(groups[6:7]).strip()
+
+
+        # set invisible parameters
         if name == 'invisible':
             name = ''
+            granica = 'white'
 
         # Person info
         guy = {
             'name' : name,
+            'radius' : radius,
+            'line_color' : line_color,
+            'line_width' : line_width,
+            'granica' : granica,
         }
 
         # Tree root
